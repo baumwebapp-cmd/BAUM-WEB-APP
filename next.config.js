d3fefcaefb2015170cd3a686883f6e8e077e4dce
KronosX38 <j.cabanas8609@gmail.com>
2026-05-12 18:09:44 -0600

    fix: correccion peso pdf

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['192.168.100.150'],
-
+  
   experimental: {
     serverActions: {
       bodySizeLimit: '52mb',
     },
   },
+
+  middlewareClientMaxBodySize: 52 * 1024 * 1024, // 52MB en bytes
 
   images: {
     remotePatterns: [
